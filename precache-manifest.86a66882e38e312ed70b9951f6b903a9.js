c9a6ebb9f5cfd16eed3f4fc514d97cd1eb43442b
sofiiastrebeleva <sofiia.strebeleva@gmail.com>
2020-10-05 15:58:23 +0300

    Updates

---
+++ precache-manifest.86a66882e38e312ed70b9951f6b903a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68785e5abc6b875459ebf42bda09c86",
+    "url": "/Todo-List--React-Hooks/index.html"
+  },
+  {
+    "revision": "6a7468e15679683be9bb",
+    "url": "/Todo-List--React-Hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "05395edb76a48b7aab4c",
+    "url": "/Todo-List--React-Hooks/static/js/2.7fe8c492.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-List--React-Hooks/static/js/2.7fe8c492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7468e15679683be9bb",
+    "url": "/Todo-List--React-Hooks/static/js/main.d3eba7b5.chunk.js"
+  },
+  {
+    "revision": "dc969e6f6ece8358355d",
+    "url": "/Todo-List--React-Hooks/static/js/runtime-main.738ffe6f.js"
+  }
+]);
